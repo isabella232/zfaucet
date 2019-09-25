d3999993af789591d4792849b41c8587b960509d
Ben Wilson <ben@z.cash>
2019-09-25 13:58:35 -0400

    Add django init step to entrypoint

--- docker/zfaucet/entrypoint.sh
+++ docker/zfaucet/entrypoint.sh
@@ -5,4 +5,5 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
+python manage.py healthcheck
 gunicorn --bind=0.0.0.0:8000 --workers=2 zfaucet.wsgi
